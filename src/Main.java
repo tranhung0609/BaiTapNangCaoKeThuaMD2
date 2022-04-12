a92109c81c09388e3ae86b2a9ba50066791819c1
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-04-12 18:00:38 +0700

    .

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(8.5, 4.4, 7.5);
+        System.out.println(triangle);
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,32 @@
+public class Triangle extends MyShape {
+    private double side1 = 1;
+    private double side2 = 1;
+    private double side3 = 1;
+
+    public Triangle() {
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getPrimeter() {
+        return (side1 + side2 + side3);
+    }
+
+    public double getArea() {
+        double p = getPrimeter() / 2;
+        return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "side1=" + side1 +
+                ", side2=" + side2 +
+                ", side3=" + side3 +
+                ", Area =" + getArea() + ", Primeter" + getPrimeter() + '}';
+    }
+}
